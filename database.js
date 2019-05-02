cb16886fd22f43a5d3076c22f0ca8f7475ae5b5a
Hamza Amer <hamer1563@gmail.com>
2019-05-01 21:44:04 -0400

    Add files via upload

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-const URI = 'mongodb+srv://heyrio:AKYSQqcxTU2Z3Ey0@cluster0-7cpbz.mongodb.net/test?retryWrites=true'
+const URI = 'mongodb+srv://heyrio:pass@cluster0-7cpbz.mongodb.net/test?retryWrites=true'
 
 mongoose.connect(URI, {useNewUrlParser: true}).then(()=>{
     console.log('Database connected...')
